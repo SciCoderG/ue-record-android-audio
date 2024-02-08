2f4d046add602bfbbcebe03aeb8f9e6036323691
David Liebemann <david@scico.dev>
2024-02-08 14:07:24 +0100

    Empty project with Odin Plugin

---
+++ RecordAudio/Source/RecordAudio/RecordAudio.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RecordAudio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RecordAudio, "RecordAudio" );
